84c63ac08c05637fce279c432bca1ea5443e58ea
Stephane renou <srenou@west.com>
2018-04-10 16:01:33 -0600

    Final check in

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,44 +1,54 @@
 package io.pivotal.pal.tracker;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryTimeEntryRepository implements TimeEntryRepository {
-
-     private HashMap<Long,TimeEntry> myMap;
-    public InMemoryTimeEntryRepository() {
-        myMap = new HashMap<Long,TimeEntry>();
-    }
+    private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
 
     @Override
     public TimeEntry create(TimeEntry timeEntry) {
+        Long id = timeEntries.size() + 1L;
+        TimeEntry newTimeEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
 
-        TimeEntry mytimeEntry = new TimeEntry(myMap.size()+1, timeEntry.getId(), timeEntry.getUserId(), timeEntry.getDate(),timeEntry.getHours() );
-         myMap.put(Long.valueOf(myMap.size())+1,mytimeEntry);
-        return mytimeEntry;
+        timeEntries.put(id, newTimeEntry);
+        return newTimeEntry;
     }
 
     @Override
     public TimeEntry find(Long id) {
-        return myMap.get(id);
+        return timeEntries.get(id);
     }
 
     @Override
     public List<TimeEntry> list() {
-        return new ArrayList<>(myMap.values());
+        return new ArrayList<>(timeEntries.values());
     }
 
     @Override
     public TimeEntry update(Long id, TimeEntry timeEntry) {
-        TimeEntry mytimeEntry = new TimeEntry(id, timeEntry.getId(), timeEntry.getUserId(), timeEntry.getDate(),timeEntry.getHours() );
-         myMap.put(id,mytimeEntry);
-        return mytimeEntry;
+        TimeEntry updatedEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
+
+        timeEntries.replace(id, updatedEntry);
+        return updatedEntry;
     }
 
     @Override
     public void delete(Long id) {
-        myMap.remove(id);
+        timeEntries.remove(id);
     }
 }
+
--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -9,31 +9,48 @@ public class TimeEntry {
     private LocalDate date;
     private int hours;
 
+    public TimeEntry() {
+    }
+
+    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
     public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
         this.id = id;
-        this.projectId=projectId;
-        this.userId=userId;
-        this.date=date;
-        this.hours=hours;
-    }
-    public TimeEntry(long id, long userId, LocalDate date, int hours) {
-        this.id = id;
-        this.projectId=0;
-        this.userId=userId;
-        this.date=date;
-        this.hours=hours;
-    }
-    public TimeEntry() {
-        this.id = 0;
-        this.projectId=0;
-        this.userId=0;
-        this.date= LocalDate.now();
-        this.hours=0;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
     }
 
     public long getId() {
         return id;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -67,40 +84,5 @@ public class TimeEntry {
                 ", date='" + date + '\'' +
                 ", hours=" + hours +
                 '}';
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(long projectId) {
-        this.projectId = projectId;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
     }
 }
